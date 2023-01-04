2a044c879ccbbdb200dca17083231cc1ccf39f55
bzhezinsyi <bzhezinskyiillia@gmail.com>
2023-01-04 14:55:09 +0200

    refactoring code Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,44 +1,39 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 
 import { RemoveScroll } from 'react-remove-scroll';
 import PropTypes from 'prop-types';
 
-export default class Modal extends Component {
-  static propTypes = {
-    onModal: PropTypes.shape({
-      largeImageURL: PropTypes.string.isRequired,
-      tags: PropTypes.string.isRequired,
-    }).isRequired,
-    onCloseModal: PropTypes.func.isRequired,
-  };
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleCloseModal);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleCloseModal);
-  }
+export default function Modal({
+  onModal: { largeImageURL, tags },
+  onCloseModal,
+}) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleCloseModal);
 
-  handleCloseModal = event => {
-    if (event.code === 'Escape') {
-      this.props.onCloseModal();
+    return () => {
+      window.removeEventListener('keydown', handleCloseModal);
+    };
+  });
+
+  const handleCloseModal = event => {
+    if (event.code === 'Escape' || event.target === event.currentTarget) {
+      onCloseModal();
     }
   };
-  handleClickCloseModal = event => {
-    if (event.target === event.currentTarget) {
-      this.props.onCloseModal();
-    }
-  };
-  render() {
-    const {
-      onModal: { largeImageURL, tags },
-    } = this.props;
 
-    return (
-      <RemoveScroll className="Overlay" onClick={this.handleClickCloseModal}>
-        <div className="Modal">
-          <img src={largeImageURL} alt={tags} />
-        </div>
-      </RemoveScroll>
-    );
-  }
+  return (
+    <RemoveScroll className="Overlay" onClick={handleCloseModal}>
+      <div className="Modal">
+        <img src={largeImageURL} alt={tags} />
+      </div>
+    </RemoveScroll>
+  );
 }
+
+Modal.propTypes = {
+  onModal: PropTypes.shape({
+    largeImageURL: PropTypes.string.isRequired,
+    tags: PropTypes.string.isRequired,
+  }).isRequired,
+  onCloseModal: PropTypes.func.isRequired,
+};
